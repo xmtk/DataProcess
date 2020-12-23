a6f2b618318fbeb4f15ba84a4604a7698b62914b
v-zhi.li <Cc123456>
2020-12-23 17:16:44 +0800

    Initial commit

---
+++ DataProcess/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def hello(request):
+    return HttpResponse("Hello world ! ")
+
+def index(request):
+    return render(request, 'index.html')
